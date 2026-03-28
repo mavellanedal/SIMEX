a1cb49117f9eb7a7e4aa221e529c7c68c8dc0c43
Chenxiao Xu <cxu2223@politecnics.barcelona>
2026-03-28 22:56:39 +0100

    feat: create model
    - Role
    - Company
    - User

--- apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/models/User.kt
+++ apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/models/User.kt
@@ -1,6 +1,7 @@
 package com.mygdx.primelogistics.android.models
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.google.gson.annotations.SerializedName
 
 data class User(
@@ -15,17 +16,24 @@ data class User(
     val identificationCardPath: String?
 =======
 import android.R
+=======
+import com.google.gson.annotations.SerializedName
+>>>>>>> c4ce83d (feat: create model)
 
 data class User(
-    val id : Int,
+    val id: Int,
     val email: String,
-    val password: String,
-    val firstname: String,
-    val lastname : String,
-    val rolId: Int,
-    val companyId: Int,
+    val nombre: String,
+    val rol: Role,
+    val company: Company?,
     val username: String,
+<<<<<<< HEAD
     val active : R.bool,
     val identificationCardPaths: String
 >>>>>>> 988cd88 (feat: create user.kt)
+=======
+    val active: Boolean,
+    @SerializedName("identification_card_path")
+    val identificationCardPath: String?
+>>>>>>> c4ce83d (feat: create model)
 )
