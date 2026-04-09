ea21de70c273301fe01f938070c37d4e525165df
sgomezl <sgomezl2425@politecnics.barcelona>
2026-04-09 17:42:05 +0200

    update items

--- apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/ClientHomeActivity.kt
+++ apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/ClientHomeActivity.kt
@@ -1,7 +1,12 @@
 package com.mygdx.primelogistics.android
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class ClientHomeActivity  : AppCompatActivity() {
+
+    private lateinit var recyclerOperations: RecyclerView
+    private lateinit var adapter: operationAdapter
+    private val operations: MutableList<Operation> = mutableListOf()
 
 }
