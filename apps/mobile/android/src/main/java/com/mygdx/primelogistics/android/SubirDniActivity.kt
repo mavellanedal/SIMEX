e94f7573c015e4dc192c2e458a854f1a9660a58f
Chenxiao Xu <cxu2223@politecnics.barcelona>
2026-04-13 18:37:30 +0200

    feat: create pickDocument

--- apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/SubirDniActivity.kt
+++ apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/SubirDniActivity.kt
@@ -1,10 +1,14 @@
 package com.mygdx.primelogistics.android
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,12 +17,28 @@ import com.mygdx.primelogistics.R
 class SubirDniActivity : AppCompatActivity() {
     private lateinit var btnHomeUsuario: ImageButton
     private lateinit var btnVolver: Button
+    private lateinit var btnSubirDNI: ImageButton
+    private lateinit var tvSelectedFile: TextView
+    private var selectedUri: Uri? = null
+
+
+
+    private val pickDocument = registerForActivityResult(
+        ActivityResultContracts.OpenDocument()
+    ) { uri ->
+        if (uri != null) {
+            selectedUri = uri
+            tvSelectedFile.text = getFileName(uri) ?: "Archivo seleccionado"
+
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_subir_dni)
-        var currentUser = intent.getSerializableExtra("user_id")
+        val currentUser = intent.getIntExtra("user_id", -1)
         defineComponents()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainActivitySubirDni)) { v, insets ->
@@ -34,15 +54,37 @@ class SubirDniActivity : AppCompatActivity() {
         btnVolver.setOnClickListener {
             volverAUsuario()
         }
+
+        btnSubirDNI.setOnClickListener {
+            pickDocument.launch(arrayOf("image/*"))
+        }
     }
 
     private fun defineComponents() {
         btnHomeUsuario = findViewById(R.id.btnHomeUsuario)
         btnVolver = findViewById(R.id.btnVolve)
+        btnSubirDNI = findViewById(R.id.btnSubirDNI)
+        tvSelectedFile = findViewById(R.id.tvNombreArchivo)
+
     }
 
     private fun volverAUsuario() {
         startActivity(Intent(this, UsuarioActivity::class.java))
         finish()
     }
+
+    private fun getFileName(uri: Uri): String? {
+        var fileName: String? = null
+
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            if (it.moveToFirst() && nameIndex >= 0) {
+                fileName = it.getString(nameIndex)
+            }
+        }
+
+        return fileName
+    }
+
 }
