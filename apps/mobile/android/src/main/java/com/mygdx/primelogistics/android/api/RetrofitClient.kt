de85717f5d816ecc57d570b35ed176b2eec6cad9
Chenxiao Xu <cxu2223@politecnics.barcelona>
2026-04-24 19:55:08 +0200

    Update RetrofitClient.kt

--- apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/api/RetrofitClient.kt
+++ apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/api/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:5085/"
+    private const val BASE_URL = "http://simex-dotnet-fohrzp-5dced4-51-83-192-177.traefik.me/"
 
     private var tokenProvider: (() -> String?)? = null
 
