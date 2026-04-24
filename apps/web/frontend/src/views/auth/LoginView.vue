66602cd68327fc567cf787a7a6fab3230cdc5fb2
Miquel Avellaneda <mavellanedal2223@politecnics.barcelona>
2026-04-24 16:31:28 +0200

    Minor changes

--- apps/web/frontend/src/views/auth/LoginView.vue
+++ apps/web/frontend/src/views/auth/LoginView.vue
@@ -38,6 +38,14 @@
               {{ showPassword ? 'visibility_off' : 'visibility' }}
             </span>
           </button>
+          <button
+            type="button"
+            @click="showPassword = !showPassword"
+            class="absolute right-3 top-[35px] text-gray-500 hover:text-gray-700 focus:outline-none"
+            title="Mostrar/Ocultar contraseña"
+          >
+            <span class="material-symbols-outlined text-xl">{{ showPassword ? 'visibility_off' : 'visibility' }}</span>
+          </button>
         </div>
         <div class="pt-4">
           <BaseButton type="submit" class="w-full">
