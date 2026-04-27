498d10eecf117ad680ba30f3c16f3fb12980e1c5
sgomezl <sgomezl2425@politecnics.barcelona>
2026-04-27 17:52:00 +0200

    Update AgentHomeActivity.kt

--- apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/AgentHomeActivity.kt
+++ apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/AgentHomeActivity.kt
@@ -2,6 +2,11 @@ package com.mygdx.primelogistics.android
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
